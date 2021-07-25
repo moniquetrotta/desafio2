01935b6342b2db201146c05ece054f5ae4dd5661
moniquetrotta <monique.trotta@gmail.com>
2021-07-24 21:44:43 -0300

    server change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-baseURL: 'https://my-json-server.typicode.com/moniquetrotta/desafio22/produtos'
+baseURL: 'https://my-json-server.typicode.com/moniquetrotta/desafio2/produtos'
 })
 export default api;
